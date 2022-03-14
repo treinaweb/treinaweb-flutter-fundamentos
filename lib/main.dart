ce03a47b51a98fbb60e7d35140e3548b987573b9
arielsardinha <odararepresentacoes@outlook.com.br>
2022-03-14 16:00:10 -0300

    validacao de formulario

--- lib/main.dart
+++ lib/main.dart
@@ -17,71 +17,129 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var quantidadePessoaController = TextEditingController();
+  var valorContaController = TextEditingController();
+  var valorPorPessoa = "Informe seus dados";
+  var formKey = GlobalKey<FormState>();
+
+  void calcular() {
+    double quantidadePessoa = double.parse(quantidadePessoaController.text);
+    double valorDaConta = double.parse(valorContaController.text);
+    double resultado = valorDaConta / quantidadePessoa;
+    setState(() {
+      valorPorPessoa = "O valor por pessoa é $resultado";
+    });
+  }
+
+  void clear() {
+    setState(() {
+      valorPorPessoa = "Informe seus dados";
+      quantidadePessoaController.text = '';
+      valorContaController.text = '';
+    });
+  }
+
+  String? validator(String? value) {
+    if (value?.isEmpty ?? false) {
+      return "O campo é obrigatório";
+    } else {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text("Raxa conta")),
+        actions: [
+          IconButton(
+              onPressed: () {
+                clear();
+              },
+              icon: const Icon(Icons.refresh_outlined))
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 25, bottom: 20),
-                child: Icon(
-                  Icons.monetization_on,
-                  size: 120,
-                  color: Colors.blue,
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 25, bottom: 20),
+                  child: Icon(
+                    Icons.monetization_on,
+                    size: 120,
+                    color: Colors.blue,
+                  ),
                 ),
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: "Quantidade de pessoa",
-                  labelStyle: TextStyle(color: Colors.blue),
+                TextFormField(
+                  controller: quantidadePessoaController,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    return validator(value);
+                  },
+                  decoration: const InputDecoration(
+                    labelText: "Quantidade de pessoa",
+                    labelStyle: TextStyle(color: Colors.blue),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: "Valor da conta",
-                  labelStyle: TextStyle(color: Colors.blue),
+                TextFormField(
+                  controller: valorContaController,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    return validator(value);
+                  },
+                  decoration: const InputDecoration(
+                    labelText: "Valor da conta",
+                    labelStyle: TextStyle(color: Colors.blue),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ),
                 ),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 25),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Calcular",
-                      style: TextStyle(
-                        color: Colors.white,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        var isValid = formKey.currentState?.validate() ?? false;
+                        if (isValid) {
+                          calcular();
+                        }
+                      },
+                      child: const Text(
+                        "Calcular",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const Text(
-                "Valor por pessoa",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18,
-                ),
-              )
-            ],
+                Text(
+                  valorPorPessoa,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
