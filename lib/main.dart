93e4d0c368ee276e5c8d3b9260ee1c6b3bb41184
arielsardinha <odararepresentacoes@outlook.com.br>
2022-03-14 11:47:46 -0300

    executando o aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() {
@@ -14,45 +22,63 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Raxa conta"),
       ),
-      body: Column(
-        children: [
-          const Icon(
-            Icons.monetization_on,
-            size: 120,
-            color: Colors.blue,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              labelText: "Quantidade de pessoa",
-              labelStyle: TextStyle(color: Colors.blue),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.monetization_on,
+                  size: 120,
+                  color: Colors.blue,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Quantidade de pessoa",
+                    labelStyle: TextStyle(color: Colors.blue),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Valor da conta",
+                    labelStyle: TextStyle(color: Colors.blue),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Calcular",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Valor por pessoa",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ),
+                )
+              ],
             ),
           ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              labelText: "Valor da conta",
-              labelStyle: TextStyle(color: Colors.blue),
-            ),
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.blue,
-              fontSize: 18,
-            ),
-          ),
-          OutlinedButton(
-            onPressed: () {},
-            child: const Icon(Icons.add),
-          ),
-          const Text(
-            "Valor por pessoa",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 18,
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
